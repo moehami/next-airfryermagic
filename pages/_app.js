a413d32e7e7aa0ceabdb108c78666cac1ad6c02e
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 22:10:03 +0300

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+// pages/_app.js
+import Head from 'next/head';
+import { Roboto_Flex } from 'next/font/google';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../public/assets/web/assets/mobirise-icons2/mobirise2_rnd1729249664608.css';
+import '../public/assets/parallax/jarallax_rnd1729249664608.css';
+import '../public/assets/socicon/css/styles_rnd1729249664608.css';
+import '../public/assets/css/mbr-additional_rnd1729249664608.css';
+import '../public/assets/dropdown/css/style_rnd1729249664608.css';
+import '../styles/globals.css'
+
+const roboto = Roboto_Flex({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+
+function Application({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default Application
