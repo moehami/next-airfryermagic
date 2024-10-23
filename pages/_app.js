095ed42c74e1187b42eb5a00b8f71a2cc2735372
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 20:53:47 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,7 @@ import '../public/assets/css/mbr-additional_rnd1729249664608.css';
 import '../public/assets/dropdown/css/style_rnd1729249664608.css';
 //import '../styles/globals.css'
 
-export const metadata: Metadata = {
-  title: "Air Fryer Magic",
-  description: "Discover the ultimate air fryer hacks and delicious recipes that will transform your cooking experience",
-}
+
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
