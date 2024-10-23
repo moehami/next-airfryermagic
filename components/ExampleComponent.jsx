1a2de1d830396144c596d416be2b60893ff3a8ce
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 20:59:37 +0300

    Update ExampleComponent.jsx

--- components/ExampleComponent.jsx
+++ components/ExampleComponent.jsx
@@ -1,6 +1,6 @@
 // ExampleComponent.jsx
-
-let metadata: Metadata = {
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
   title: "Air Fryer Magic",
   description: "Discover the ultimate air fryer hacks and delicious recipes that will transform your cooking experience",
 }
