0821aa31f836f221415d5162d10f1a88ebdb5ac1
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 13:04:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import ExampleComponent from '../components/ExampleComponent';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to My Next.js App</h1>
+
+
       <ExampleComponent />
-    </div>
+
   );
 }
