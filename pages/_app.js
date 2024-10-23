aa5e05155301efcd8d24958b24f64be6dc0893c4
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 20:51:11 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,8 @@ import '../public/assets/dropdown/css/style_rnd1729249664608.css';
 //import '../styles/globals.css'
 
 export const metadata: Metadata = {
-  title: 'Air Fryer Magic',
-  description: 'Discover the ultimate air fryer hacks and delicious recipes that will transform your cooking experience'
+  title: "Air Fryer Magic",
+  description: "Discover the ultimate air fryer hacks and delicious recipes that will transform your cooking experience",
 }
 
 function Application({ Component, pageProps }) {
