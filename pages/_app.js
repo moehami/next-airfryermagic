e8ccbdefb47835751a3d6c64b96ba4800b68596b
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 14:44:08 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@public/assets/web/assets/mobirise-icons2/mobirise2_rnd1729249664608.css';
+import styles from '../assets/web/assets/mobirise-icons2/mobirise2_rnd1729249664608.css';
 import './assets/parallax/jarallax_rnd1729249664608.css';
 import '/assets/socicon/css/styles_rnd1729249664608.css';
 import '../assets/css/mbr-additional_rnd1729249664608.css';
