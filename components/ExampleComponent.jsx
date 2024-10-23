a1b07867f48f203af0c019e652112282ebd7754d
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 21:03:43 +0300

    Update ExampleComponent.jsx

--- components/ExampleComponent.jsx
+++ components/ExampleComponent.jsx
@@ -1,9 +1,9 @@
 // ExampleComponent.jsx
-import type { Metadata } from 'next',
+import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: "Air Fryer Magic",
   description: "Discover the ultimate air fryer hacks and delicious recipes that will transform your cooking experience",
-}
+};
 const ExampleComponent = () => {
   return (
     <div>
