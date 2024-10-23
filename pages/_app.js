7f14c1f112aae2733963f027ff9737fed16de10d
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 15:03:50 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import '../assets/parallax/jarallax_rnd1729249664608.css';
 //import '../assets/socicon/css/styles_rnd1729249664608.css';
 //import '../assets/css/mbr-additional_rnd1729249664608.css';
-import '../globals.css'
+import 'public/styles/globals.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
