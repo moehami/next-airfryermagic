53fb41a84b9b78238fed252dd9cf13551fe663ba
moehami <m.aljaaly@yahoo.co.uk>
2024-10-23 22:30:59 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import Head from 'next/head';
 import { Roboto_Flex } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import '../public/assets/bootstrap/css/bootstrap-grid.min_rnd%3D1729249664608.css';
-import '../public/assets/bootstrap/css/bootstrap.min_rnd%3D1729249664608.css';
-import '../public/assets/bootstrap/css/bootstrap.reboot.min_rnd%3D1729249664608.css';
+import '../public/assets/bootstrap/css/bootstrap-grid.min_rnd1729249664608.css';
+import '../public/assets/bootstrap/css/bootstrap.min_rnd1729249664608.css';
+import '../public/assets/bootstrap/css/bootstrap.reboot.min_rnd1729249664608.css';
 import '../public/assets/web/assets/mobirise-icons2/mobirise2_rnd1729249664608.css';
 import '../public/assets/parallax/jarallax_rnd1729249664608.css';
 import '../public/assets/socicon/css/styles_rnd1729249664608.css';
