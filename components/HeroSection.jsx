00100c8d0f1b6e7c53c95dbb8f14d6b82a6c9d6e
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 03:25:10 +0300

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,3 +1,32 @@
 // components/HeroSection.jsx
 export default function HeroSection() {
   return (
+  <section
+    data-bs-version="5.1"
+    className="header14 cid-uryiHhZpv0 mbr-parallax-background"
+    id="call-to-action-1-uryiHhZpv0"
+  >
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="card col-12 col-md-12 col-lg-8">
+          <div className="card-wrapper">
+            <div className="card-box align-center">
+              <h1 className="card-title mbr-fonts-style mb-4 display-1">
+                <strong>Join the Frying Revolution Today!</strong>
+              </h1>
+              <div className="mbr-section-btn mt-4">
+                <a
+                  className="btn btn-primary display-7"
+                  href="https://aflover.vercel.app/"
+                >
+                  Explore Now
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+    );
+}
