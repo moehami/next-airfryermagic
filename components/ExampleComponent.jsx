9e2e50c9f322e47e3fa382e071128237b969d432
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 04:25:22 +0300

    Update ExampleComponent.jsx

--- components/ExampleComponent.jsx
+++ components/ExampleComponent.jsx
@@ -33,60 +33,7 @@ const ExampleComponent = () => {
 
 
 
-  <section
-    data-bs-version="5.1"
-    className="image02 cid-uryiHilmKb mbr-fullscreen mbr-parallax-background"
-    id="image-13-uryiHilmKb"
-  >
-    <div className="container">
-      <div className="row" />
-    </div>
-  </section>
-  <section
-    data-bs-version="5.1"
-    className="social4 cid-uryiHilHB7"
-    id="follow-us-1-uryiHilHB7"
-  >
-    <div className="container">
-      <div className="media-container-row">
-        <div className="col-12">
-          <h3 className="mbr-section-title align-center mb-5 mbr-fonts-style display-2">
-            <strong>Join Our Air Fryer Family</strong>
-          </h3>
-          <div className="social-list align-center">
-            <a
-              className="iconfont-wrapper bg-facebook m-2 "
-              target="_blank"
-              href="#"
-            >
-              <span className="socicon-facebook socicon" />
-            </a>
-            <a
-              className="iconfont-wrapper bg-twitter m-2"
-              href="#"
-              target="_blank"
-            >
-              <span className="socicon-twitter socicon" />
-            </a>
-            <a
-              className="iconfont-wrapper bg-instagram m-2"
-              href="#"
-              target="_blank"
-            >
-              <span className="socicon-instagram socicon" />
-            </a>
-            <a
-              className="iconfont-wrapper bg-tiktok m-2"
-              href="#"
-              target="_blank"
-            >
-              <span className="socicon-tiktok socicon" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+
   <section
     data-bs-version="5.1"
     className="form5 cid-uryiHimeIg"
