e63348035a5802f8265a834b143eb07cb2a5a7d7
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 20:25:23 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import useSWR from "swr";
+import useSWR from 'swr';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -8,24 +8,36 @@ export default function Home() {
     isLoading,
     isError: error,
   } = useSWR(
-    "https://jsonplaceholder.typicode.com/comments?_limit=6",
+    'https://jsonplaceholder.typicode.com/comments?_limit=6',
     fetcher,
     { revalidateOnFocus: false, revalidateOnReconnect: false }
   );
 
   if (error) {
-    return <p>Failed to fetch</p>;
-  }
-
-  if (isLoading) {
-    return <p>Loading comments...</p>;
+    return <p className='text-secondary'>Failed to fetch</p>;
   }
 
   return (
-       <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>
-              {comment.name}
-            </li>
-          ))}
-        </ul>
+    <>
+      <header className='p-7 text-center bg-primary shadow-md'>
+        <h1 className='text-secondary font-bold text-xl'>Comments App</h1>
+      </header>
+      <main className='max-w-[1100px] mx-auto mt-10 pb-10 px-4'>
+        {isLoading ? (
+          <p className='text-secondary'>Loading comments</p>
+        ) : (
+          <ul className='grid grid-cols-3 gap-x-3 gap-y-3'>
+            {comments.map((comment, index) => (
+              <li className='bg-primary p-10 rounded-lg shadow-md' key={index}>
+                <span className='block mb-4 font-bold text-lg text-secondary capitalize'>
+                  {comment.name}
+                </span>
+                <span className='text-secondary'>{comment.body}</span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
+    </>
+  );
+}
