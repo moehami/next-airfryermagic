78a491fd4232e646341085f4e147806b5b94aced
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 03:53:12 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,15 @@ import HomePage from '../components/ExampleComponent';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import CallToAction from '../components/CallToAction';
+import AboutSection from '../components/AboutSection';
+
 export default function Home() {
   return (
     <>
       <Navbar />
     <HeroSection />
     <CallToAction />
+    <AboutSection />
       <HomePage />
     </>
   );
