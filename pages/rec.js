27cfb86e514a0d7ad44098d10efc2e2cc7fa8e2b
moehami <m.aljaaly@yahoo.co.uk>
2024-10-25 01:07:14 +0300

    Update and rename about.js to rec.js

---
+++ pages/rec.js
@@ -0,0 +1,27 @@
+// pages/index.js
+import { useEffect, useState } from 'react';
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: '9z46zrgh5qec',
+  accessToken: 'KzOIik4H_2cON-AliK4ROe8txuHoDWelHPVKsnWDXNM'
+});
+
+const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    client.getEntries({ content_type: 'recipe' })
+      .then((response) => setRecipes(response.items));
+  }, []);
+
+  return (
+    <div>
+      {recipes.map((item) => (
+        <div key={item.sys.id}>{item.fields.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
