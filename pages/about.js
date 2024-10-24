7ababfd8fa637a618272d149a969a1cde91e4200
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 14:39:21 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,21 @@
 "use Client";
-import Layout from '../components/layout'
-import card from 'react-bootstrap'
+import Card from 'react-bootstrap/Card';
 
-export default function About() {
-  return ( <div class="card">
-  <div class="card-body">
-    This is some text within a card body.
-  </div>
-</div>);
+function TextExample() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
 }
-    
+
+export default TextExample;
