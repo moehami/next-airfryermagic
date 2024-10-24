b4a248e8e30d93b313b5cbc82e00d05b2186d712
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 03:37:55 +0300

    Create HeroSection1.jsx

---
+++ components/HeroSection1.jsx
@@ -0,0 +1,26 @@
+export default function HeroSection() {
+  return (
+    <section className="header16 cid-uryiHhZg34 mbr-fullscreen mbr-parallax-background">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="content-wrap col-12 col-md-12">
+            <h1 className="mbr-section-title mbr-fonts-style mbr-white mb-4 display-1">
+              <strong>Fry Like A Pro</strong>
+            </h1>
+            <p className="mbr-fonts-style mbr-text mbr-white mb-4 display-7">
+              Unlock the secrets to crispy perfection with our air fryer magic!
+            </p>
+            <div className="mbr-section-btn">
+              <a
+                className="btn btn-white-outline display-7"
+                href="https://aflover.vercel.app/"
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
