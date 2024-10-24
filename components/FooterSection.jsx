b0f634179f089c1c97c3de5d52fc2acab25129c2
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 04:45:30 +0300

    Create FooterSection.jsx

---
+++ components/FooterSection.jsx
@@ -0,0 +1,74 @@
+export default function FooterSection() {
+  return (
+<section
+    data-bs-version="5.1"
+    className="footer3 cid-uryiHimvKA"
+    once="footers"
+    id="footer-3-uryiHimvKA"
+  >
+    <div className="container">
+      <div className="row">
+        <div className="row-links">
+          <ul className="header-menu">
+            <li className="header-menu-item mbr-fonts-style display-5">
+              <a href="#" className="text-white">
+                Home
+              </a>
+            </li>
+            <li className="header-menu-item mbr-fonts-style display-5">
+              <a href="#" className="text-white">
+                Recipes
+              </a>
+            </li>
+            <li className="header-menu-item mbr-fonts-style display-5">
+              <a href="#" className="text-white">
+                Hacks
+              </a>
+            </li>
+            <li className="header-menu-item mbr-fonts-style display-5">
+              <a href="#" className="text-white">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 mt-4">
+          <div className="social-row">
+            <div className="soc-item">
+              <a href="https://aflover.vercel.app/" target="_blank">
+                <span className="mbr-iconfont socicon socicon-facebook display-7" />
+              </a>
+            </div>
+            <div className="soc-item">
+              <a href="#" target="_blank">
+                <span className="mbr-iconfont socicon-twitter socicon" />
+              </a>
+            </div>
+            <div className="soc-item">
+              <a href="#" target="_blank">
+                <span className="mbr-iconfont socicon-instagram socicon" />
+              </a>
+            </div>
+            <div className="soc-item">
+              <a href="#" target="_blank">
+                <span className="mbr-iconfont socicon socicon-linkedin" />
+              </a>
+            </div>
+            <div className="soc-item">
+              <a href="#" target="_blank">
+                <span className="mbr-iconfont socicon socicon-twitch" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 mt-5">
+          <p className="mbr-fonts-style copyright display-7">
+            {" "}
+            2024 Air Fryer Magic. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+}
