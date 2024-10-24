c5b21e4d03e2449c8dfe2ce492642173eefe11d5
moehami <m.aljaaly@yahoo.co.uk>
2024-10-24 06:08:22 +0300

    Update JoinusSection.jsx

--- components/JoinusSection.jsx
+++ components/JoinusSection.jsx
@@ -9,32 +9,5 @@ export default function JoinusSection() {
         <div className="row" />
       </div>
     </section>
-
-    <section
-      data-bs-version="5.1"
-      className="social4 cid-uryiHilHB7"
-      id="follow-us-1-uryiHilHB7"
-    >
-      <div className="container">
-        <div className="media-container-row">
-          <div className="col-12">
-            <h3 className="mbr-section-title align-center mb-5   
- mbr-fonts-style display-2">
-              <strong>Join Our Air Fryer Family</strong>
-            </h3>
-            <div className="social-list align-center">
-              <a
-                className="iconfont-wrapper bg-facebook m-2 "
-                target="_blank"
-                href="#"
-              >
-                <span className="socicon-facebook socicon" />
-              </a>
-              {/* ... other social media links */}
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
   );
 }
