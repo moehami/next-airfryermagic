cbbe0e710961b75bc7554492cbf257adba864853
moehami <m.aljaaly@yahoo.co.uk>
2024-10-25 01:02:26 +0300

    Create call1.js

---
+++ pages/call1.js
@@ -0,0 +1,11 @@
+const contentful = require('contentful')
+
+const client = contentful.createClient({
+  space: '9z46zrgh5qec',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: 'KzOIik4H_2cON-AliK4ROe8txuHoDWelHPVKsnWDXNM'
+})
+
+client.getEntry('011oZBbAf2Xms9L4UnaCdq')
+  .then((entry) => console.log(entry))
+  .catch(console.error)
