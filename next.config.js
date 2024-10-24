9c70343e21285396b20b426f598269d8d8cb8f6c
moehami <m.aljaaly@yahoo.co.uk>
2024-10-25 01:33:52 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    // Fixes npm packages that depend on 'fs' module
+    config.node = {
+      fs: 'empty'
+    };
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
