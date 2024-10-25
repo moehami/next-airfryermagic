3119fc889a05a595d38c0df61b600095b3970b8e
moehami <m.aljaaly@yahoo.co.uk>
2024-10-25 19:19:35 +0300

    Update page.jsx

--- pages/recipe/[slug]/page.jsx
+++ pages/recipe/[slug]/page.jsx
@@ -1,17 +1,39 @@
-type BlogPageProps = {
-  params: {
-    slug: string;
-  };
+import Link from "next/link";
+import { BlogQueryResult } from "./types";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+});
+
+const getBlogEntries = async (): Promise<BlogQueryResult> => {
+  const entries = await client.getEntries({ content_type: "blog" });
+  return entries;
 };
 
-export default async function BlogPage(props: BlogPageProps) {
-  const { params } = props;
-  const { slug } = params;
+export default async function Home() {
+  const blogEntries = await getBlogEntries();
   return (
-    <main className="min-h-screen p-24 flex justify-center">
-      <div className="max-w-2xl">
-        <h1>you are in {slug}</h1>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {blogEntries.items.map((singlePost) => {
+        const { slug, title, date } = singlePost.fields;
+        return (
+          <div key={slug}>
+            <Link href={`/articles/${slug}`}>
+              <h2>{title}</h2>
+              <span>
+                Posted on{" "}
+                {new Date(date).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </span>
+            </Link>
+          </div>
+        );
+      })}
     </main>
   );
 }
